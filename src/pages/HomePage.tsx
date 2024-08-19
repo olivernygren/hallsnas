4737b0fde6723d05622ffd7f891e03e3e3c3f0e0
Oliver Nygren <oliver@jobnet.se>
2024-08-19 19:42:32 +0200

    Password protect page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
 import theme from '../theme';
 import PageHeader from '../components/header/Header';
@@ -12,22 +12,51 @@ import OtherInfoSection from '../sections/OtherInfo';
 import FindSection from '../sections/Find';
 import QuestionFormSection from '../sections/QuestionForm';
 import Footer from '../sections/Footer';
+import { HeadingsTypography, NormalTypography } from '../components/typography/Typography';
+
+const passwordKey = 'abcdefgh';
 
 const HomePage = () => {
+  const [password, setPassword] = useState('');
+  
+  if (window.location.href.includes(passwordKey)) {
+    return (
+      <RootContainer>
+        <PageHeader />
+        <Hero />
+        <AboutSection />
+        <TimesSection />
+        <AccommodationSection />
+        <ActivitiesSection />
+        <FoodAndDrinkSection />
+        <OtherInfoSection />
+        <FindSection />
+        <QuestionFormSection />
+        <Footer />
+      </RootContainer>
+    )
+  }
+
   return (
-    <RootContainer>
-      <PageHeader />
-      <Hero />
-      <AboutSection />
-      <TimesSection />
-      <AccommodationSection />
-      <ActivitiesSection />
-      <FoodAndDrinkSection />
-      <OtherInfoSection />
-      <FindSection />
-      <QuestionFormSection />
-      <Footer />
-    </RootContainer>
+    <PasswordContainer>
+      <NormalTypography>Ange lösenord för att få åtkomst till sidan</NormalTypography>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        if (password === passwordKey) {
+          window.location.href = `${window.location.href}?${passwordKey}`;
+        } else {
+          alert('Fel lösenord');
+        }
+      }}>
+        <input 
+          type="text" 
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Bekräfta</button>
+      </form>
+    </PasswordContainer>
   )
 }
 
@@ -36,6 +65,17 @@ const RootContainer = styled.div`
   min-height: 200vh;
   position: relative;
   overflow-x: hidden;
+`;
+
+const PasswordContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: ${theme.spacing.s};
 `;
 
 export default HomePage;
